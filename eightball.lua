7f79b60656d72cce5ce3290751516dc0bd1f84aa
glock <74268710+glockcodes@users.noreply.github.com>
2021-10-11 13:33:03 -0400

    Update eightball.lua

--- eightball.lua
+++ eightball.lua
@@ -6,15 +6,15 @@
 local tab, container = 'LUA', 'B'
 
 local answers = {
-    "Yes - definitely.",
-    "It is decidedly so.",
-    "Without a doubt.",
-    "Reply hazy, try again.",
-    "Ask again later.",
-    "Better not tell you now.",
-    "My sources say no.",
-    "Outlook not so good.",
-    "Very doubtful."
+    'Yes - definitely.',
+    'It is decidedly so.',
+    'Without a doubt.',
+    'Reply hazy, try again.',
+    'Ask again later.',
+    'Better not tell you now.',
+    'My sources say no.',
+    'Outlook not so good.',
+    'Very doubtful.'
 }
 
 local _ = ui.new_label(tab, container, 'GameSense 8BALL')
