fa7a95cf09f284ab13ea09bc1198b2806793d261
glock <74268710+glockcodes@users.noreply.github.com>
2021-12-29 03:11:44 -0500

    Recoded the script properly

--- hp_based_freestanding.lua
+++ hp_based_freestanding.lua
@@ -1,24 +1,40 @@
---[[
-    HP Based Freestanding
-    - This allows you to disable freestanding if your health is below the number on the slider.
-    - I created this before my conditions lua so that is why it is a thread.
-]]
-
 local tab, container = 'AA', 'Anti-aimbot angles'
 
-local hpbase_button = ui.new_checkbox(tab, container, 'HP Based Freestanding')
-local hpbase_slider = ui.new_slider(tab, container, 'Disabler HP', 1, 100, 1, true, 'hp')
+local references = {
+	freestanding = ui.reference(tab, container, 'Freestanding')
+}
 
-local freestanding = ui.reference(tab, container, 'Freestanding')
+local interface = {
+	enabled = ui.new_checkbox(tab, container, 'Health based freestanding'),
+	disabler = ui.new_slider(tab, container, 'Disabler health', 1, 100, 1, true, 'hp')
+}
 
-client.set_event_callback('paint_ui', function()
-    local local_hp = entity.get_prop(entity.get_local_player(), 'm_iHealth')
+local on_run_cmd = function(cmd)
+	local local_player = entity.get_local_player()
+	local is_alive = entity.is_alive(local_player)
 
-    if ui.get(hpbase_button) then
-        ui.set(freestanding, 'Default')
+	if not local_player or not is_alive then return end
 
-        if ui.get(hpbase_slider) > local_hp then
-            ui.set(freestanding, '-')
-        end
+	local health = entity.get_prop(local_player, 'm_iHealth')
+	local disabler = ui.get(interface.disabler)
+
+	ui.set(references.freestanding, 'Default')
+
+    if disabler >= health then
+        ui.set(references.freestanding, '-')
     end
+end
+
+local handle_callback = function(event)
+	local handle = event and client.set_event_callback or client.unset_event_callback
+
+	handle('run_command', on_run_cmd)
+	handle('shutdown', function()
+		ui.set(references.freestanding, '-')
+	end)
+end
+
+ui.set_callback(interface.enabled, function()
+	local enabled = ui.get(interface.enabled)
+	handle_callback(enabled)
 end)
